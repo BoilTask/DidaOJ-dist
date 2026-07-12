70308163828bbc0ccb4267609595c071e87ca17d
BoilTask <BoilTask@qq.com>
2026-07-12 19:14:10 +0800

    增加按需加载机制

---
+++ assets/library-B-yn7JJl.js
@@ -0,0 +1,1 @@
+function o(t){return t?Math.floor(t/1e9):0}function e(t){if(t<=0)return"0 秒 ";let r="";const i=Math.floor(t/3600);i>0&&(r+=`${i} 小时 `);const f=Math.floor(t%3600/60);return(f>0||i>0)&&(i>0&&f<10&&(r+="0"),r+=`${f} 分钟 `),t=t%60,(t>0||f>0||i>0)&&((i>0||f>0)&&t<10&&(r+="0"),r+=`${t.toFixed(0)} 秒`),r}export{e as G,o as a};
---
+++ assets/message-BfVNsthn.js
@@ -0,0 +1,1 @@
+const s=1,e=2,a=3,t=-1;export{t as A,s as M,a,e as b};
