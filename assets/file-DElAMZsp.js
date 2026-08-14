9be67bb0c125c488f9b91b7534da1dff590a96f2
BoilTask <BoilTask@qq.com>
2026-08-14 17:19:44 +0800

    build: update dist (AI testdata standalone page + judge page slim-down)

---
+++ assets/file-DElAMZsp.js
@@ -0,0 +1,2 @@
+function r(t){if(t==null||t<=0)return"0 B";let e;return t>10*1024?t>1024*1024?e=(t/(1024*1024)).toFixed(3)+" MB":e=(t/1024).toFixed(3)+" KB":e=t+" B",e}export{r as G};
+//# sourceMappingURL=file-DElAMZsp.js.map
